e35a33b3e47430f271c742e3f29b17bcf7f165dc
Szymon Dolnik <szymon2407@hotmail.com>
2024-07-15 21:28:21 +0200

    feat: Add Widgets

--- src/service/models.ts
+++ src/service/models.ts
@@ -7,7 +7,26 @@ export type NavigationItem = {
 
 export type Navigation = NavigationItem[];
 
-export type Widget = {};
+export type ImageWidget = {
+  type: "ImageWidget";
+  image: {
+    url: string;
+    alt: string;
+  };
+};
+
+export type TextWidget = {
+  type: "TextWidget";
+  text: string; // includes HTML
+};
+
+export type TwoTextColsWidget = {
+  type: "TwoTextColsWidget";
+  leftText: string; // includes HTML
+  rightText: string; // includes HTML
+};
+
+export type Widget = ImageWidget | TextWidget | TwoTextColsWidget;
 
 export type Page = {
   id: string;
