fcc99b4de20cf2cc4227ed58f7699bf52ac3fc20
Szymon Dolnik <szymon2407@hotmail.com>
2024-07-16 13:27:40 +0200

    refactor: Change TwoTextColsWidget implementation to vue

---
+++ src/components/widgets/TwoTextColsWidget.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { TwoTextColsWidget } from "../../service/models";
+
+defineProps<TwoTextColsWidget>();
+</script>
+
+<template>
+  <section class="two-text-cols-widget">
+    <div v-html="leftText" />
+    <div v-html="rightText" />
+  </section>
+</template>
+
+<style scoped>
+.two-text-cols-widget {
+  --columns: 1;
+
+  display: grid;
+  grid-template-columns: repeat(var(--columns), 1fr);
+  gap: 50px;
+  width: 100%;
+  max-width: 1000px;
+  margin-inline: auto;
+
+  @media (min-width: 768px) {
+    --columns: 2;
+  }
+}
+</style>
