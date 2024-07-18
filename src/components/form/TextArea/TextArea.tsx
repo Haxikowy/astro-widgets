c50d603dc9ded974010dbb4f8cef67521854630c
Szymon Dolnik <szymon2407@hotmail.com>
2024-07-18 17:11:59 +0200

    feat: Add hook form implementation to inputs

--- src/components/form/TextArea/TextArea.tsx
+++ src/components/form/TextArea/TextArea.tsx
@@ -1,18 +1,30 @@
 import { clsx } from "clsx";
 import { useId, type TextareaHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
 import styles from "./TextArea.module.scss";
 
 interface TextInputProps
   extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, "id"> {
   label: string;
+  name: string; // This is required for react-hook-form
 }
 
 export default function TextArea({
   label,
+  name,
   className,
   ...props
 }: TextInputProps) {
   const id = useId();
+
+  const methods = useFormContext();
+  if (!methods) throw new Error("TextArea must be used within FormWrapper");
+
+  const {
+    register,
+    formState: { errors },
+  } = methods;
+  const error = errors?.[name]?.message;
 
   return (
     <div className={styles["text-area"]}>
@@ -21,11 +33,14 @@ export default function TextArea({
       </label>
       <div className={styles["input-wrapper"]}>
         <textarea
+          {...register(name)}
           id={id}
           className={clsx(styles.input, className)}
+          aria-invalid={!!error}
           {...props}
         />
       </div>
+      {error && <span className={styles["error-message"]}>{`${error}`}</span>}
     </div>
   );
 }
--- src/components/form/TextInput/TextInput.tsx
+++ src/components/form/TextInput/TextInput.tsx
@@ -1,18 +1,30 @@
 import { clsx } from "clsx";
 import { useId, type InputHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
 import styles from "./TextInput.module.scss";
 
 interface TextInputProps
-  extends Omit<InputHTMLAttributes<HTMLInputElement>, "id"> {
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "id" | "name"> {
   label: string;
+  name: string; // This is required for react-hook-form
 }
 
 export default function TextInput({
   label,
+  name,
   className,
   ...props
 }: TextInputProps) {
   const id = useId();
+
+  const methods = useFormContext();
+  if (!methods) throw new Error("TextArea must be used within FormWrapper");
+
+  const {
+    register,
+    formState: { errors },
+  } = methods;
+  const error = errors?.[name]?.message;
 
   return (
     <div className={styles["text-input"]}>
@@ -20,8 +32,15 @@ export default function TextInput({
         {label}
       </label>
       <div className={styles["input-wrapper"]}>
-        <input id={id} className={clsx(styles.input, className)} {...props} />
+        <input
+          {...register(name)}
+          id={id}
+          className={clsx(styles.input, className)}
+          aria-invalid={!!error}
+          {...props}
+        />
       </div>
+      {error && <span className={styles["error-message"]}>{`${error}`}</span>}
     </div>
   );
 }
